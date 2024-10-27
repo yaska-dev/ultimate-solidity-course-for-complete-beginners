68a0836894e071531dda850a88c150b6b9242c71
yaasiinaxmed <yaskassoy@gmail.com>
2024-10-27 13:31:55 +0300

    Done Basic Twitter Exercise

--- Exercises/7-11-BasicTwitter-Exercise.sol
+++ Exercises/7-11-BasicTwitter-Exercise.sol
@@ -2,13 +2,25 @@
 
 pragma solidity ^0.8.0;
 
-// 1️⃣ Create a Twitter Contract 
-// 2️⃣ Create a mapping between user and tweet 
-// 3️⃣ Add function to create a tweet and save it in mapping
-// 4️⃣ Create a function to get Tweet 
-// 5️⃣ Add array of tweets 
+// 1️⃣ Create a Twitter Contract: Done
+// 2️⃣ Create a mapping between user and tweet: Done
+// 3️⃣ Add function to create a tweet and save it in mapping: Done
+// 4️⃣ Create a function to get Tweet: Done
+// 5️⃣ Add array of tweets: Done
 
-contract Twitter {
+contract Twiiter {
 
+    mapping(address => string[]) public tweets;
 
+    function createTweet(string memory _tweet) public {
+        tweets[msg.sender].push(_tweet);
+    }
+
+    function getTweet(address _owner, uint _i) public view returns (string memory) {
+        return tweets[_owner][_i];
+    }
+
+    function getTweets(address _owner) public view returns (string[] memory) {
+        return tweets[_owner];
+    }
 }
