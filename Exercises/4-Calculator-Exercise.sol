b6b4660baa337a58530bee60a9ebb40a75a73792
yaasiinaxmed <yaskassoy@gmail.com>
2024-10-27 13:40:26 +0300

    updae calculator exercise

--- Exercises/4-Calculator-Exercise.sol
+++ Exercises/4-Calculator-Exercise.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-// 1️⃣ Make a contract called Calculator
-// 2️⃣ Create Result variable to store result
-// 3️⃣ Create functions to add, subtract, and multiply to result
-// 4️⃣ Create a function to get result
+// 1️⃣ Make a contract called Calculator: Done
+// 2️⃣ Create Result variable to store result: Done
+// 3️⃣ Create functions to add, subtract, and multiply to result: Done
+// 4️⃣ Create a function to get result: Done
 
 contract Calculator {
     uint256 public result;
